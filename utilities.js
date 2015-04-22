c1060fdbcf9ae705d26063160fee115286ea0c0f
Daniel Dalton <daniel.dalton10@gmail.com>
2015-04-22 14:27:42 +1000

    * Add code to produce a map and position points on a grid. Unsure to how   accurate this works currently. * placeSearch code merged with mapConstructor.js so placeSearch.js can   be removed. * Add a function to utilities to find the place which is the furthest   from a given point. Returns the index of the place and the distance. * Export functions in utilities so they can be used else where.

--- utilities.js
+++ utilities.js
@@ -1,8 +1,8 @@
-function toRadians (deg) {
+toRadians = function (deg) {
     return deg/180.0 * Math.PI;
 }
 
-function distance(location1, location2) {
+distance = function (location1, location2) {
     const R = 6372800; // earth radius in metres
     var lat1 = toRadians(location1.lat);
     var  lon1 = toRadians(location1.lng);
@@ -13,11 +13,11 @@ function distance(location1, location2) {
     var a = Math.sin(dLat / 2) * Math.sin(dLat /2) + Math.sin(dLon / 2) * Math.sin(dLon /2) * Math.cos(lat1) * Math.cos(lat2);
     var c = 2 * Math.asin(Math.sqrt(a));
     return R * c;
-}
+};
 
-function initialBearing (location1, location2) {
+initialBearing = function (location1, location2) {
     return (bearingDegrees(location1, location2) + 360) % 360;
-}
+};
 
 function bearingDegrees (location1, location2) {
     var lat1 = toRadians(location1.lat);
@@ -28,3 +28,21 @@ function bearingDegrees (location1, location2) {
         Math.cos(lat1)*Math.sin(lat2) - Math.sin(lat1)*Math.cos(lat2)*Math.cos(lng2-lng1)
     ) * 180/Math.PI;
 }
+
+findMaxDistance = function (centre, places) {
+    var maxDistance = -1;
+    var index = -1;
+    for (var i = 0; i < places.length; i++) {
+	var currentDistance = distance(centre, places[i].geometry.location);
+	if (currentDistance > maxDistance) {
+	    index = i;
+	    maxDistance = currentDistance;
+	}
+    }
+    return {index:index,distance:maxDistance};
+};
+
+exports.distance = distance;
+exports.initialBearing = initialBearing;
+exports.findMaxDistance = findMaxDistance;
+exports.toRadians = toRadians;
