3f8c0aeae3e25fb094f9ed2e8746a8b8b30ac35c
Daniel Dalton <daniel.dalton10@gmail.com>
2015-06-18 20:28:38 +1000

    code comments for utilities module

--- utilities.js
+++ utilities.js
@@ -22,14 +22,14 @@
 
 /**
  * This module provides miscellaneous functions that are useful in the
- *  wider code.
-*/
+ * wider code.
+ */
 
 /**
  * Convert an angle in degrees to radians.
  * @param deg A number representing an angle in degrees.
  * @return The number representing the angle in radians.
-*/
+ */
 toRadians = function (deg) {
     return deg/180.0 * Math.PI;
 }
@@ -41,7 +41,7 @@ toRadians = function (deg) {
  * @param location2 An object representing the second location with 
  *  "lat" and "lng" properties.
  * @return The distance between the points in metres.
-*/
+ */
 distance = function (location1, location2) {
     const R = 6372800; // earth radius in metres
     var lat1 = toRadians(location1.lat);
@@ -58,12 +58,12 @@ distance = function (location1, location2) {
 /**
  * Calculate the initial bearing between two locations.
  * @param location1 The location which the bearing is taken from. Must
- *  contain lat and lng properties.
+ * contain lat and lng properties.
  * @param location2 The location at which the bearing is being sort for
- *  i.e. the end point. Must have lat and lng properties. 
+ * i.e. the end point. Must have lat and lng properties. 
  * @return An angle representing the initial bearing in degrees between 0
- *  and 360
-*/
+ * and 360
+ */
 initialBearing = function (location1, location2) {
     return (bearingDegrees(location1, location2) + 360) % 360;
 };
@@ -71,12 +71,12 @@ initialBearing = function (location1, location2) {
 /**
  * Calculate the initial bearing between two locations.
  * @param location1 The location which the bearing is taken from. Must
- *  contain lat and lng properties.
+ * contain lat and lng properties.
  * @param location2 The location at which the bearing is being sort for
- *  i.e. the end point. Must have lat and lng properties. 
+ * i.e. the end point. Must have lat and lng properties. 
  * @return An angle representing the initial bearing in degrees which
- *  may be outside the range 0 and 360.
-*/
+ * may be outside the range 0 and 360.
+ */
 function bearingDegrees (location1, location2) {
     var lat1 = toRadians(location1.lat);
     var lat2 = toRadians(location2.lat);
@@ -88,63 +88,93 @@ function bearingDegrees (location1, location2) {
 }
 
 /**
- * Convert a map object to svg.
+ * Generate the SVG for a map object.
  * @param map A Map object to generate a SVG for.
  * @return svg string for map.
-*/
+ */
 toSVG = function (map) {
     var colour = 0xFF0000;
+    // beginning svg markup
     var svg = "<svg height =\"" + map.height + "\" width=\"" + map.width
 	    + "\" xmlns=\"http://www.w3.org/2000/svg\""
 	    + " xmlns:xlink=\"http://www.w3.org/1999/xlink\">\n";
+    // beginning metadata markup 
     var metadata = "<metadata id=\"1401062652130\" title=\""+makeTitle(map)+"\""
 	    + " description=\"Automatically generated map\""
 	    + " category=\"map\">\n"
 	    + "<summary>\n" + buildDescription(map) + "\n"
 	    + "</summary>\n";
 
+    // add polylines first (roads)
     for (var i = 0; i < map.mapLines.length; i++) {
 	svg += svgPolyShape("polyline", map.mapLines[i], colour);
 	metadata += addMetadata(map.mapLines[i], colour);
 	++colour;
     }
 
+    // add polygons next (buildings)
     for (var i = 0; i < map.mapPolygons.length; i++) {
 	svg += svgPolyShape("polygon", map.mapPolygons[i], colour);
 	metadata += addMetadata(map.mapPolygons[i], colour);
 	++colour;
     }
 
+    // add points
     for (var i = 0; i < map.mapPoints.length; i++) {
 	svg += svgCircle (map.mapPoints[i], colour);
 	metadata += addMetadata(map.mapPoints[i], colour);
 	++colour;
     }
 
+    // collaborate and return svg 
     metadata += "  </metadata>\n";
     svg += metadata + "</svg>";
     return svg;
 };
 
+/**
+ * Generate a SVG circle element.
+ * @param point The point where the circle should be placed. Has x and y
+ * coordinate and a name as keys in the js object.
+ * @param colour Integer value which represents the colour of the point. 
+ * @return The svg markup to produce this svg circle.
+ */
 function svgCircle (point, colour) {
     var svg = "<circle id=\"" + point.id + "\" cx=\"" + Math.round(point.x) + "\" cy=\"" 
 	+ Math.round(point.y) + "\" r=\"40\" stroke=\"#"+colour.toString(16)+"\" stroke-width=\"10\" fill=\"#"+colour.toString(16)+"\" />\n";
     return svg;
 }
 
+/**
+ * Produce SVG markup for a polygon or polyline.
+ * @param type Either polygon or polyline. 
+ * @param polyShape A PolyShape object which represents a line or
+ *  polygon to plot on the SVG.
+ * @param colour Integer value for the colour of the polyshape in the
+ * svg.
+ * @return The svg of the polyshape. 
+ */
 function svgPolyShape (type, polyShape, colour) {
     var svg = "<" + type + " id=\"" + polyShape.id + "\" points=\"";
+    // concat each pair of coordinates to the svg string 
     for (var j = 0; j < polyShape.coordinates.length; j+=2) {
 	svg += Math.round(polyShape.coordinates[j]) + "," +
 	    Math.round(polyShape.coordinates[j+1]);
-	if (j+2 < polyShape.coordinates.length) {
-	    svg += " ";
+	if (j+2 < polyShape.coordinates.length) { // is this last coordinates
+	    svg += " "; // no so add a space
 	}
     }
     svg += "\" style=\"stroke:#"+colour.toString(16)+"\" stroke-width=\"20\" fill=\"#"+colour.toString(16)+"\"/>\n";
     return svg;
 }
 
+/**
+ * Add the GraViewer metadata for a SVG object.
+ * @param point A javascript object with {x,y and name} keys which
+ * describes a point or polyshape on the SVG.
+ * @param colour The colour of the svg object.
+ * @return Return the metadata string.
+ */
 function addMetadata (point, colour) {
     var metadata = "    <gravvitas>\n"
 	    + "<id>" + point.id + "</id>\n"
@@ -167,27 +197,43 @@ function addMetadata (point, colour) {
     return metadata;
 }
 
+/**
+ * Compile a brief description of a map object for the svg graphic.
+ * @param map The map object.
+ * @return A string which summarises this map.
+ */
 function buildDescription (map) {
     // todo count streets and POI correctly, but this should be a good
     // indication for now.
     // for instance mapLines can refer to roads, railway lines, tram tracks
     // and cycle ways. 
     var roads = new Array();
+    // add unique roads to the array
     for (var i = 0; i < map.mapLines.length; i++) {
 	if (roads.indexOf(map.mapLines[i].name) < 0) {
+	    // road hasn't been counted yet so add it to the array
 	    roads[roads.length] = map.mapLines[i].name;
 	}
     }
+    // points of interest is the sum of circles and polygons on the map.
+    // roads.length is the number of streets on the map as collected above
     return "This map is centred around " + map.centre.name +"." 
 	+ " It contains " + roads.length + " streets, and "
 	+ (map.mapPoints.length + map.mapPolygons.length) + " points of"
 	+ " interest.";
 }
 
+/**
+ * Return the title for a map. This includes the name of the centre
+ *  point and its dimensions.
+ * @param map The map object that requires a title. 
+ * @return A title string for the map.
+ */
 function makeTitle (map) {
     return map.centre.name + " " + map.height + " by " + map.width;
 }
 
+// export functions for other modules 
 exports.distance = distance;
 exports.initialBearing = initialBearing;
 exports.toRadians = toRadians;
